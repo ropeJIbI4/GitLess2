4f786b90186dd705fe7ed7a4c30996fe3d7b5b92
Alex <pogi199411@gmail.com>
2022-08-07 16:18:34 +0500

    ImputData

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿using System;
 using static System.Console;
+WriteLine("Введите размер массива!");
+int size = int.Parse(ReadLine());
+string [] arrayA = new string [size];
+WriteLine("Введите данные через пробел");
+arrayA = (ReadLine()).Split(' ',StringSplitOptions.RemoveEmptyEntries);
+WriteLine($"Ваши данные:{String.Join(" ",arrayA)}");
