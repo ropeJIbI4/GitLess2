3ec94b5680c8d0a7e6238aa214c18795799dac1c
Alex <pogi199411@gmail.com>
2022-08-07 16:58:15 +0500

    Add Method

--- Program.cs
+++ Program.cs
@@ -6,3 +6,17 @@ string [] arrayA = new string [size];
 WriteLine("Введите данные через пробел");
 arrayA = (ReadLine()).Split(' ',StringSplitOptions.RemoveEmptyEntries);
 WriteLine($"Ваши данные:{String.Join(" ",arrayA)}");
+WriteLine($"Данные после выполнения Алгоритма:{String.Join(" ",GetArrayDoCondition(arrayA))}");
+
+string [] GetArrayDoCondition(string [] arr )
+{
+string [] result = new string [arr.Length];
+for (int i = 0; i < arr.Length; i++)
+{
+ if (arr[i].Length<=3)
+ {
+    result[i] = Convert.ToString(arr[i]);
+ }
+}
+return result;
+}
